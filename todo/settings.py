8f444fe0682cf9a993150b559dd21b4935fdb81c
helizaga <tom.elizaga@gmail.com>
2020-05-18 16:52:12 -0700

    update

--- todo/settings.py
+++ todo/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crevfgkc',
+        'USER': 'crevfgkc',
+        'PASSWORD': 'Y0BINR5nI7epXi3wFFeGL-nmsYuwDcwT',
+        'HOST': 'rajje.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
